63bf28c534a38fe96693add83d28639c81efdcb7
Femi <oluwafemi.za@gmail.com>
2023-01-20 06:44:01 +0100

    More restructuring

--- app/src/main/java/com/femi/lasustudentreg/data/presentation/RegistrationFormEvent.kt
+++ app/src/main/java/com/femi/lasustudentreg/data/presentation/RegistrationFormEvent.kt
@@ -5,7 +5,6 @@ sealed class RegistrationFormEvent{
     data class EmailChanged(val email: String): RegistrationFormEvent()
     data class FacultyChanged(val faculty: String): RegistrationFormEvent()
     data class JambScoreChanged(val jambScore: String): RegistrationFormEvent()
-    data class MatricChanged(val matric: String): RegistrationFormEvent()
     data class FirstNameChanged(val firstName: String): RegistrationFormEvent()
     data class LastNameChanged(val lastName: String): RegistrationFormEvent()
 
--- app/src/main/java/com/femi/lasustudentreg/ui/navigation/Navigation.kt
+++ app/src/main/java/com/femi/lasustudentreg/ui/navigation/Navigation.kt
@@ -6,7 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.femi.lasustudentreg.ui.UserDetailsScreen
 import com.femi.lasustudentreg.ui.WelcomeScreen
-import com.femi.lasustudentreg.ui.viewmodel.MainActivityViewModel
+import com.femi.lasustudentreg.viewmodel.MainActivityViewModel
 
 @Composable
 fun Navigation(
